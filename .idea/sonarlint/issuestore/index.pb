db7406a02dc99c3d2a49c33d298c8d7c0ff3de5b
lijiefeng <2212489627@qq.com>
2021-10-22 10:31:40 +0800

    add

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+‡
+Wuser-service/user-web/src/main/java/com/game/user/web/controller/AccountController.java,0/9/09d11367e1fd3783bc4edce3a8298db900f2fdaa
+E
+order-service/pom.xml,d/3/d3bf53ecc442740843266a635cbfe9807b992566
+O
+order-service/order-api/pom.xml,5/f/5fcc10a50832c958da82e2e761729274dff62fa7
+O
+order-service/order-web/pom.xml,d/5/d5f9b632350d32ea4bcf9a886cfcbef684d6a491
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
--- user-service/user-web/src/main/java/com/game/user/web/service/impl/UserServiceImpl.java
+++ user-service/user-web/src/main/java/com/game/user/web/service/impl/UserServiceImpl.java
@@ -4,11 +4,20 @@ package com.game.user.web.service.impl;
 import com.game.user.web.service.UserService;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.*;
+
 /**
  * 用户功能业务实现
  * @author lijf
  */
 @Service
 public class UserServiceImpl implements UserService {
+
+    private static ThreadPoolExecutor execThreadpool = new ThreadPoolExecutor(50, 300,
+            1L, TimeUnit.SECONDS,
+            new LinkedBlockingQueue<>(100),
+            new ThreadPoolExecutor.CallerRunsPolicy()
+    );
+
 
 }
